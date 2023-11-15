220838c72af8e22f397e4f93bee8db8e03674350
Christopher Lozzi <clozzi12@gmail.com>
2023-11-15 16:03:20 -0500

    added alt text for logo accessibility

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import logo from "./logo192.png"
 
 function Footer() {
     return (
-        <footer>Created with React<br/><img src={logo} /></footer>
+        <footer>Created with React<br/><img src={logo} alt="React logo"/></footer>
     )
 };
 
