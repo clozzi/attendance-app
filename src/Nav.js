181384b72475d82095f2a21f9a25ba84ffd313b2
Christopher Lozzi <clozzi12@gmail.com>
2023-11-20 13:06:37 -0500

    NavLink isActive styling

--- src/Nav.js
+++ src/Nav.js
@@ -4,9 +4,21 @@ function Nav() {
     return (
         <header>
             <nav>
-                <NavLink to="/home" style={{padding: 5}}>Home</NavLink>
-                <NavLink to="/about" style={{padding: 5}}>About</NavLink>
-                <NavLink to="/newStudentForm" style={{padding: 5}}>New Student</NavLink>
+                <NavLink to="/home" style={({isActive}) => ({
+                    padding: 5,
+                    color: isActive ? '#003300' : '#6699ff',
+                    background: isActive ? '#6699ff' : '#e6f2ff',
+                })}>Home</NavLink>
+                <NavLink to="/about" style={({isActive}) => ({
+                    padding: 5,
+                    color: isActive ? '#003300' : '#6699ff',
+                    background: isActive ? '#6699ff' : '#e6f2ff',
+                })}>About</NavLink>
+                <NavLink to="/newStudentForm" style={({isActive}) => ({
+                    padding: 5,
+                    color: isActive ? '#003300' : '#6699ff',
+                    background: isActive ? '#6699ff' : '#e6f2ff',
+                })}>New Student</NavLink>
             </nav>
         </header>
         
