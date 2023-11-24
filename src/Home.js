a876d756632c7f2b441b1da8767404b1e5f6d4a5
Christopher Lozzi <clozzi12@gmail.com>
2023-11-24 14:30:08 -0500

    added profile photo to first student

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,10 @@ import { useOutletContext } from "react-router-dom";
 
 function Home() {
     const [students, addStudent] = useOutletContext();
+    console.log(addStudent)
 
     const studentList = students.map(student => (
-            <li key={student.id}>{student.name}</li>
+            <li key={student.id}>{student.name} <br /><img src={student.image} alt="student" style={{height: 100, width: 100}}></img></li>
         ))
 
     return (
