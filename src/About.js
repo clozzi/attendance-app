58b77c702a6b6c016e2bec708c9bae0e9fbeb47a
Christopher Lozzi <clozzi12@gmail.com>
2023-11-28 19:33:36 -0500

    removed console.logs

--- src/About.js
+++ src/About.js
@@ -2,7 +2,6 @@ import { useOutletContext } from "react-router-dom";
 
 function About() {
     const [students, addStudent] = useOutletContext();
-    console.log(addStudent)
 
     const studentList = students.map(student => (
         <li key={student.id}>{student.name}<br />Preferred Language: {student.language}<br />Learning Style: {student.learningStyle}</li>
